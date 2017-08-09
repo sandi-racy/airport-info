73c3d23df20683abc5ebfd71bdd356ee236643f9
Sandi Rosyandi <sandi.rosyandi@aitindo.com>
2017-08-09 23:50:53 +0700

    Add facebook login

--- www/src/js/components/Login.vue
+++ www/src/js/components/Login.vue
@@ -6,7 +6,7 @@
         <div class="row">
           <div class="col s12 m12">
             <div class="card-action text-center">
-              <a class="waves-effect waves-light blue btn" @click="login"><i class="material-icons left"></i>Login with Facebook</a>
+              <a class="waves-effect waves-light blue btn" @click="doLogin"><i class="material-icons left"></i>Login with Facebook</a>
             </div>
           </div>
         </div>
@@ -16,8 +16,31 @@
 
 <script>
     export default {
+        data() {
+            return {
+                login: {
+                    data: {
+                        fb_uid: 1,
+                        email: 'team@hydra.com',
+                        name: 'Team Hydra'
+                    }
+                }
+            }
+        },
+
         methods: {
-            login: function () {
+            doLogin() {
+                this.$http.post(ai.api.url + 'signin', this.login.data).then(() => {
+                    localStorage.setItem('fb_uid', this.login.data.fb_uid)
+                    this.$router.push('order')
+                }, () => {
+                    alert('Can\'t login to Facebook')
+                })
+            }
+        },
+
+        mounted() {
+            if (localStorage.getItem('fb_uid')) {
                 this.$router.push('order')
             }
         }
