cbac308530fd783311967d095dcdc5741a470361
Muslim Al Fatih <muslimalfatih22@gmail.com>
2017-08-10 23:54:00 +0700

    update styling view page

--- www/src/js/components/View.vue
+++ www/src/js/components/View.vue
@@ -5,27 +5,123 @@
           <h1>Airport Info</h1>
         </div>
       </div>
-      <div class="sign-up">
-        <div class="row">
-          <form class="col s12">
-            <div class="sign-up__name">
-              <h2>Hi, Muslim</h2>
-            </div>
-
-            <div class="row">
-              <div class="input-field col s12">
-                <label for="phone">Phone Number: 082 211 222 222</label>
-              </div>
-            </div>
-          </form>
+      <div class="card--view">
+        <div class="profile-view">
+          <div class="profile-view__image">
+            <span>M</span>
+          </div>
+          <div class="profile-view__content">
+            <p>Muslim Al Fatih</p>
+            <p>Jakarta</p>
+          </div>
         </div>
+      </div>
+
+      <div class="card--info">
+        <h3>Personal Info</h3>
+        <div class="card--info__name">
+          <dl>
+            <dt>Name</dt>
+            <dd>Muslim Al Fatih</dd>
+          </dl>
+        </div>
+
+        <div class="card--info__phone">
+          <dl>
+            <dt>Phone</dt>
+            <dd>082 272 222 222</dd>
+          </dl>
+        </div>
+
+        <div class="card--info__category">
+          <dl>
+            <dt>Category</dt>
+            <dd>Banking</dd>
+          </dl>
+        </div>
+
+        <div class="card--info__notification">
+          <dl>
+            <dt>Notification</dt>
+            <dd>SMS</dd>
+          </dl>
+        </div>
+
+        <!-- <div class="card--info__edit">
+          <p>Edit Details</p>
+        </div> -->
       </div>
     </div>
 </template>
 
 <style lang="scss">
-  .hero {
-    // background: url('../../img/bg.png')
+  .order-page {
+    padding-bottom: 40px
+  }
+
+  .card {
+    &--info,
+    &--view {
+      width: 80%;
+      margin: 0 auto;
+      padding: 15px 20px;
+      border-radius: 5px;
+      background-color: #fff;
+      box-shadow: 0 2px 5px rgba(100,100,100,.175);
+    }
+
+    &--view {
+      position: relative;
+      top: -45px;
+    }
+
+    &--info {
+      margin-top: -25px;
+
+      h3 {
+        margin-top: 10px;
+        font-size: 1.4rem;
+      }
+
+      dt {
+        font-size: 0.9rem;
+        color: #757575;
+      }
+
+      dd {
+        margin-left: 0;
+      }
+    }
+  }
+  .profile-view {
+    display: flex;
+
+    &__image {
+      margin-right: 15px;
+      width: 50px;
+      height: 50px;
+      line-height: 47px;
+      border-radius: 50%;
+      border: 2px solid #9e9e9e;
+      text-align: center;
+      font-size: 17px;
+      font-weight: 500;
+
+      span {
+        color: #2196f3;
+      }
+    }
+
+    &__content {
+      padding: 5px 0;
+
+      p {
+        margin: 0;
+        &:last-of-type {
+          font-size: 12px;
+        }
+      }
+    }
   }
 
 </style>
