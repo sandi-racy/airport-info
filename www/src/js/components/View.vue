af1b91f2a506e4b3d2ab6ba896e1bce21dee1c8e
Muslim Al Fatih <muslimalfatih22@gmail.com>
2017-08-10 21:47:05 +0700

    add view page

---
+++ www/src/js/components/View.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="order-page">
+      <div class="hero">
+        <div class="hero__header">
+          <h1>Airport Info</h1>
+        </div>
+      </div>
+      <div class="sign-up">
+        <div class="row">
+          <form class="col s12">
+            <div class="sign-up__name">
+              <h2>Hi, Muslim</h2>
+            </div>
+
+            <div class="row">
+              <div class="input-field col s12">
+                <label for="phone">Phone Number: 082 211 222 222</label>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+</template>
+
+<style lang="scss">
+  .hero {
+    // background: url('../../img/bg.png')
+  }
+
+</style>
